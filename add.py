6a38901c0b8758d92b89c839e7e674bf1b4bbed6
AbhiGowdaIndia <abhigowdainduvalu@gmail.com>
2022-08-29 13:38:17 +0530

    This function is eddited by Developer2

--- add.py
+++ add.py
@@ -1,1 +1,7 @@
-Hey Someone please write addition function in this.
+## Hey Someone please write addition function in this.
+
+## This function is written by Developer 2
+
+def addition(a,b):
+    return a+b
+
